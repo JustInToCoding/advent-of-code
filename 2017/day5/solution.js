690bc3d6869b9a81e5b69e4e4680888a745720f3
Justin Steenhuis <justinsteenhuis@gmail.com>
2017-12-05 11:04:37 +0100

    Recursive day 5

--- 2017/day5/solution.js
+++ 2017/day5/solution.js
@@ -2,18 +2,23 @@ const { input } = require('./input.js');
 
 const inputArray = input.split('\n');
 
-const calculateJumpsToExit = (input, modifyAfter) => {
-    let index = 0;
-    let steps = 0;
+const calculateJumpsToExit = (input, steps, index, modifyJump) => {
     while(index < input.length) {
         let currentIndex = index;
         let jump = +input[index];
         index += jump;
-        input[currentIndex] = modifyAfter(jump);
+        input[currentIndex] = modifyJump(jump);
         steps++;
     }
     return steps;
 }
 
-// console.log(calculateJumpsToExit(inputArray, (value) => ++value));
-console.log(calculateJumpsToExit(inputArray, (value) => value > 2 ? --value : ++value));
+const recursiveCalculateJumpsToExit = (input, steps, index, modifyJump) => {
+    let jump = +input[index];
+    input[index] = modifyJump(jump);
+    steps++;
+    return (index < input.length) ? calculateJumpsToExit(input, steps, index + jump, modifyJump) : steps;
+}
+
+//console.log(calculateJumpsToExit(inputArray, 0, 0, (value) => ++value));
+console.log(recursiveCalculateJumpsToExit(inputArray, 0, 0, (value) => value > 2 ? --value : ++value));
