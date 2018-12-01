7a2c5df6b879a9b4ad740d6841abdbe4c1358a17
Justin Steenhuis <JUSTIN@SGRNET.local>
2018-12-01 21:19:57 +0100

    Add stream.cycle()

--- 2018/day1/part2.exs
+++ 2018/day1/part2.exs
@@ -3,6 +3,7 @@ start = System.monotonic_time()
 File.stream!("./input.txt")
 |> Stream.map(&String.replace(&1, "\n", ""))
 |> Stream.map(&Integer.parse(&1))
+|> Stream.cycle()
 |> Enum.reduce_while({MapSet.new([0]), 0}, fn {value, _}, {map_set, frequency} ->
   new_frequency = frequency + value
 
