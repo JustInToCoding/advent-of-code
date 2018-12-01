150d15959cc46857a97eff4ded698c88ff2f8d64
Justin Steenhuis <JUSTIN@SGRNET.local>
2018-12-01 17:00:13 +0100

    Try to fix

--- 2018/day1/part2.exs
+++ 2018/day1/part2.exs
@@ -1,14 +1,20 @@
 start = System.monotonic_time()
 
+map_set = MapSet.new()
+MapSet.put(map_set, 0)
+
 File.stream!("./input.txt")
 |> Stream.map(&String.replace(&1, "\n", ""))
 |> Stream.map(&Integer.parse(&1))
-|> Enum.reduce_while([0], fn {value, _}, acc ->
-  new_head = hd(acc) + value
+|> Enum.reduce_while(0, fn {value, _}, acc ->
+  new_frequency = acc + value
 
-  if Enum.member?(acc, new_head), do: {:halt, new_head}
-
-  {:cont, [new_head | acc]}
+  if MapSet.member?(map_set, new_frequency) do
+    IO.inspect('Found: ')
+    {:halt, new_frequency}
+  else
+    {:cont, new_frequency}
+  end
 end)
 |> IO.inspect()
 
