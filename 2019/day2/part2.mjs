014c605ee59f09e3a19075cf7d4141991ac49f72
Justin <justin@linku.nl>
2019-12-03 16:44:18 +0100

    day2 part2

---
+++ 2019/day2/part2.mjs
@@ -0,0 +1,44 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('./input.txt').toString().split(',').map(value => +value);
+
+const program = (input, noun, verb) => {
+    const memory = [...input];
+    memory[1] = noun;
+    memory[2] = verb;
+
+    const processOpcode = (input, position) => {
+        const output = [...input];
+        const opcode = output[position];
+        const first = output[output[position + 1]];
+        const second = output[output[position + 2]];
+        const placementPosition = output[position + 3];
+        switch(opcode) {
+            case 1: {
+                const result = first + second;
+                output[placementPosition] = result;
+                return processOpcode(output, position + 4);
+            }
+            case 2: {
+                const result = first * second;
+                output[placementPosition] = result;
+                return processOpcode(output, position + 4);
+            }
+            case 99:
+            default:
+                return input;
+        }
+    }
+
+    const result = processOpcode(memory, 0);
+    return result[0];
+}
+
+for(let noun = 0; noun < 100; noun++) {
+    for(let verb = 0; verb < 100; verb++) {
+        const output = program(input, noun, verb);
+        if(output === 19690720) {
+            console.log(100 * noun + verb);
+        }
+    }
+}
